8e84b2c1e8b3bf5b0e4f36b8a3fcebc4f6c40247
Jon Sanchez <jonathan.sanchez@windstream.net>
2020-08-23 19:14:26 -0600

    Script Questions

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var questions = [{
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
+    answer: "<script>"
+},
+{
+    title: "What is the correct JavaScript syntax to write 'Hello World'?",
+    choices: ["response.write('Hello World')", "'Hello World'", "document.wrote('Hello World')", "(Hello World)"],
+    answer: "document.wrote('Hello World')"
+},
+{
+    title: " An external JavaScript must contain the <script> tag",
+    choices: ["True", "False"],
+    answer: "False"
+},
+{
+    title: "How do you create a function?",
+    choices: ["function:myFunction()", "function=myFunction()", "function myFunction()", "myFunction():function"],
+    answer: "function myFunction"
+},
+{
+    title: "How does a 'for' loop start?",
+    choices: ["or (i = 0; i <= 5)", "for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i <= 5; i++)"],
+    answer: "for (i = 0; i <= 5; i++)"
+}
+]
